81ff858f5ae16ad633928982f7522451942423a6
unknown <909956da@gmail.com>
2025-04-02 12:05:00 +0300

    Добавлены флаги, которые ранее были удалены

--- internal/flagsenv/flags.go
+++ internal/flagsenv/flags.go
@@ -1,6 +1,7 @@
 package flagsenv
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/caarlos0/env/v11"
@@ -20,6 +21,11 @@ var ConfAgent OptionsAgent
 var ConfServer OptionsServer
 
 func ParseFlagsAgent() (OptionsAgent, error) {
+	flag.StringVar(&ConfAgent.Hp, "a", "localhost:8080", "Адрес и порт хоста")
+	flag.IntVar(&ConfAgent.Pi, "p", 2, "Интервал опроса")
+	flag.IntVar(&ConfAgent.Ri, "r", 4, "Интервал отчётов")
+	flag.Parse()
+
 	var confAgent OptionsAgent
 	if err := env.Parse(&confAgent); err != nil {
 		fmt.Printf("Ошибка парсинга: %+v\n", err)
@@ -30,6 +36,9 @@ func ParseFlagsAgent() (OptionsAgent, error) {
 }
 
 func ParserFlagsServer() (OptionsServer, error) {
+	flag.StringVar(&ConfServer.Endpoint, "a", "localhost:8080", "input Port")
+	flag.Parse()
+
 	var confServer OptionsServer
 	if err := env.Parse(&confServer); err != nil {
 		fmt.Printf("Ошибка парсинга: %+v\n", err)
